6aeba633bc40c64aa9f09d9bac0388ab74b7d2e2
Ranjit <vishwakarmaranjit8109@gmail.com>
2025-06-21 13:40:32 +0530

    commit

--- JavaScript/CWH_06_Operators.js
+++ JavaScript/CWH_06_Operators.js
@@ -1,7 +1,7 @@
 // 44;
 // "Ranjit Vishwakarma"
 // false
-// All these three lines are valid in JS
+// All these three lines are valid in JS, it will not throw an error
 
 console.log("Operators in JS")
 
@@ -14,8 +14,8 @@ console.log("a + b = ", a+b)
 console.log("a - b = ", a-b)
 console.log("a * b = ", a*b)
 console.log("a / b = ", a/b)
-console.log("a ** b = ", a**b)  // Exponential operator, this means 45 raise to 4 means = 45x45x45x45 = 4100625
-console.log("a % b = ", a%b)    // Modulus operator, it shows the remainder
+console.log("a ** b = ", a**b)     // Exponential operator, this means 10 raise to 4 means = 10X10X10X10 = 10000
+console.log("a % b = ", a%b)       // Modulus operator, it shows the remainder
 // console.log("a++ = ", a++)      // a++ increment the value after printing the value of a (always only 1 will add)
 // console.log("a = ",a)
 // console.log("++a = ", ++a)      // ++a will increase the value of a print the value of a (always only 1 will add)
@@ -58,7 +58,7 @@ console.log("comp1 !== comp2 is ", comp1 !== comp2)
 let x = 5;
 let y = 6;
 console.log(x<y && x!=y)    // In && operator TT = T, TF = F, FT = F, FF = F
-                            // In && operator, If both conditions is True then only it will return true
+                            // In && operator, If both conditions is true then only it will return true
 console.log(x<y || x==y)    // In || (or or) operator, if any one condition is true then it will return true
 console.log(!true)          // This is not operator (!)
 console.log(!false)
