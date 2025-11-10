ffeac77e51785bf7087deda0f2eca6a239eaed9d
Ranjit vishwakarma <vishwakarmaranjit8109@gmail.com>
2025-11-10 20:03:21 +0530

    new commit

--- JS_in_browser/36_Searching_the_DOM/script.js
+++ JS_in_browser/36_Searching_the_DOM/script.js
@@ -1,19 +1,21 @@
-const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
-const appendAlert = (message, type) => {
-  const wrapper = document.createElement('div')
-  wrapper.innerHTML = [
-    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-    `   <div>${message}</div>`,
-    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-    '</div>'
-  ].join('')
+// Change the card title to red
 
-  alertPlaceholder.append(wrapper)
-}
+// let ctitle = document.getElementsByClassName("card-title")[0];
+// ctitle.style.color = "red";
 
-const alertTrigger = document.getElementById('liveAlertBtn')
-if (alertTrigger) {
-  alertTrigger.addEventListener('click', () => {
-    appendAlert('Nice, you triggered this alert message!', 'success')
-  })
-}
+// let ctitle = document.getElementById("firstcardtitle");
+// dtitle.style.color = "aqua";
+
+let ctitle = document.querySelectorAll(".card-title")
+ctitle[0].style.color = "red"
+ctitle[1].style.color = "aqua"
+ctitle[2].style.color = "green"
+
+let d = document.querySelector(".this");
+d.style.background = "red"
+d.style.color = "cyan"
+
+console.log(document.getElementsByTagName("a"))
+console.log(document.querySelector(".card").getElementsByTagName("a"))
+
+console.log(document.getElementsByName("search"))
