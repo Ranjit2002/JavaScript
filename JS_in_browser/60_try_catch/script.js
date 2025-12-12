25acb82e08db96725b76708932405a5ef704df94
Ranjit vishwakarma <vishwakarmaranjit8109@gmail.com>
2025-12-12 20:10:23 +0530

    commit

---
+++ JS_in_browser/60_try_catch/script.js
@@ -0,0 +1,31 @@
+setTimeout(() => {
+    console.log("Hacking wifi...Please wait....")
+}, 1000);
+
+try{
+        console.log(Rahul);
+}
+catch(err){
+    console.log("Balle balle" + err);
+}
+
+// try{                             // This try catch block will not work because we scheduled the code execution
+//     setTimeout(() => {           // By using setTimeout()
+//         console.log(Rahul);
+//     }, 100);
+// }
+// catch(err){
+//     console.log("Balle balle" + err);
+// }
+
+setTimeout(() => {
+    console.log("Fetching username and password...Please wait...")
+}, 2000);
+
+setTimeout(() => {
+    console.log("Hacking Rahul's facebook id...Please wait...")
+}, 3000);
+
+setTimeout(() => {
+    console.log("Username and password of Rahul (+913453464534) fetched...Please wait...")
+}, 4000);
