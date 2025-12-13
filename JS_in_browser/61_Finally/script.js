9a1e5270d4052c4b7a918a4bf9c0de477cd60c67
Ranjit vishwakarma <vishwakarmaranjit8109@gmail.com>
2025-12-13 20:13:35 +0530

    commit

---
+++ JS_in_browser/61_Finally/script.js
@@ -0,0 +1,24 @@
+const f = () => {
+
+    try {
+        let a = 0;
+        // console.log(program);
+        console.log("Program ran successflly");
+        return;
+    }
+    catch (err) {
+        console.log("This is an error");
+        console.log(p);
+    }
+    finally {
+        console.log("I am a good boy")
+        // Close the file
+        // Exit the loop
+        // Write to the log file
+    }
+
+}
+
+f();
+
+console.log("End");
