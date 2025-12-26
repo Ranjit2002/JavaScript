6b630f16e392064bf6cec6ab57a7f957960b24ed
Ranjit vishwakarma <vishwakarmaranjit8109@gmail.com>
2025-12-26 19:54:36 +0530

    commit

---
+++ JS_in_browser/78_Inheritance/script.js
@@ -0,0 +1,34 @@
+class Animal{
+
+    constructor(name, color){
+        this.name = name
+        this.color = color
+    }
+
+    run(){
+        console.log(this.name + " is running!")
+    }
+
+    shout(){
+        console.log(this.name + " is shouting!")
+    }
+}
+
+class Monkey extends Animal{
+    
+    eatBanana(){
+        console.log(this.name + " is eating banana")
+    }
+
+    hide(){
+        console.log(`${this.name} is hiding`)
+    }
+
+}
+
+let ani = new Animal("Bruno", "White")
+let m = new Monkey("Chimpu", "Orange")
+
+ani.shout();
+m.eatBanana();
+m.hide();
