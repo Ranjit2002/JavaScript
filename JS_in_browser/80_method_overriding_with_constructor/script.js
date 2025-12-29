c980c844ab160989f20eaadfd8916cb7d85aa6ce
Ranjit vishwakarma <vishwakarmaranjit8109@gmail.com>
2025-12-29 19:01:05 +0530

    commit

---
+++ JS_in_browser/80_method_overriding_with_constructor/script.js
@@ -0,0 +1,48 @@
+class Employee {
+
+    constructor(name) {
+        console.log(`${name} - Employee's constructor is here`)
+        this.name = name
+    }
+
+    login() {
+        console.log(`Employee has logged in`)
+    }
+
+    logout() {
+        console.log(`Employee has logged out`)
+    }
+
+    requestLeaves(leaves) {
+        console.log(`Employee has requested ${leaves} leaves - Auto approved`)
+    }
+
+}
+
+class Programmer extends Employee {
+
+    constructor(name) {
+        super(name)
+        console.log(`This is newly written constructor.`)
+    }
+
+    // constructor(...args){    ==> If there is no constructor in the class, this is created automitacally.
+    //     super(...args)       
+    // }
+
+    requestCoffee(x) {
+        console.log(`Employee has requested ${x} coffees`)
+    }
+
+    requestLeaves(leaves) {
+        super.requestLeaves(4);
+        console.log(`One extra is granted`)
+    }
+
+}
+
+
+let e = new Programmer("Ranjit");
+
+e.login();
+e.requestLeaves(3);
