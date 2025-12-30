252bfeafa2ed6d93570b74fd64acbcfdcaf80a4e
Ranjit vishwakarma <vishwakarmaranjit8109@gmail.com>
2025-12-30 21:02:43 +0530

    commit

---
+++ JS_in_browser/81_static_methods/script.js
@@ -0,0 +1,21 @@
+class Animal{
+
+    constructor(name){
+        this.name = Animal.capitalize(name)
+    }
+
+    walk(){
+        // alert(`Animal ${Animal.capitalize(this.name)} is walking`)
+        alert(`Animal ${this.name} is walking`)
+    }
+
+    static capitalize(name){
+        return name.charAt(0).toUpperCase() + name.substr(1, name.length)
+    }
+
+}
+
+j = new Animal("jack");
+j.walk();
+
+// console.log(j.capitalize("thisa"))    // ==> This doesn't work
