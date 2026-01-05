10c5ecd608e9cf3b48ce221f294e9b87a0c918b8
Ranjit vishwakarma <vishwakarmaranjit8109@gmail.com>
2026-01-05 20:26:59 +0530

    commit

--- JS_in_browser/85_Exercise_7/script.js
+++ JS_in_browser/85_Exercise_7/script.js
@@ -14,3 +14,136 @@ class Password{
 }
 
 */
+
+
+class Password {
+
+    constructor() {
+        this.Password = "";
+    }
+
+    generateStrongPassword(length = 8) {
+        let lower = "abcdefghijklmnopqrstuvwxyz"
+        let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        let special = "!@#$%^&*()_+[]{}<>?/";
+
+        let pass =
+            [Math.floor(Math.random * lower.length)] +
+            [Math.floor(Math.random * upper.length)] +
+            [Math.floor(Math.random * special.length)]
+
+        const all = lower + upper + special
+
+        for (i = pass.length; i < length; i++) {
+            pass += all[Math.floor(Math.random * all.length)]
+        }
+    }
+
+    generateWeakPassword() {
+
+    }
+
+    generateSuperStrongPassword() {
+
+    }
+
+    generateFunnyPassword() {
+
+    }
+
+}
+
+
+
+/*
+class Password {
+  constructor() {
+    this.password = "";
+  }
+
+  // Helper method to shuffle characters
+  shuffle(str) {
+    return str
+      .split("")
+      .sort(() => Math.random() - 0.5)
+      .join("");
+  }
+
+  generateStrongPassword(length = 8) {
+    const lower = "abcdefghijklmnopqrstuvwxyz";
+    const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const special = "!@#$%^&*()_+[]{}<>?/";
+
+    let pass =
+      lower[Math.floor(Math.random() * lower.length)] +
+      upper[Math.floor(Math.random() * upper.length)] +
+      special[Math.floor(Math.random() * special.length)];
+
+    const all = lower + upper + special;
+
+    for (let i = pass.length; i < length; i++) {
+      pass += all[Math.floor(Math.random() * all.length)];
+    }
+
+    this.password = this.shuffle(pass);
+    return this.password;
+  }
+
+  generateWeakPassword(length = 6) {
+    const chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+    let pass = "";
+    for (let i = 0; i < length; i++) {
+      pass += chars[Math.floor(Math.random() * chars.length)];
+    }
+
+    this.password = pass;
+    return this.password;
+  }
+
+  generateSuperStrongPassword(length = 12) {
+    const lower = "abcdefghijklmnopqrstuvwxyz";
+    const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numbers = "0123456789";
+    const special = "!@#$%^&*()_+[]{}<>?/";
+
+    let pass =
+      lower[Math.floor(Math.random() * lower.length)] +
+      upper[Math.floor(Math.random() * upper.length)] +
+      numbers[Math.floor(Math.random() * numbers.length)] +
+      special[Math.floor(Math.random() * special.length)];
+
+    const all = lower + upper + numbers + special;
+
+    for (let i = pass.length; i < length; i++) {
+      pass += all[Math.floor(Math.random() * all.length)];
+    }
+
+    this.password = this.shuffle(pass);
+    return this.password;
+  }
+
+  generateFunnyPassword() {
+    const funnyPasswords = [
+      "ILoveJS😂",
+      "NoBugPls!",
+      "404NotFound!",
+      "Coffee>Sleep☕",
+      "HackMeIfYouCan😎"
+    ];
+
+    this.password =
+      funnyPasswords[Math.floor(Math.random() * funnyPasswords.length)];
+    return this.password;
+  }
+}
+
+/* Usage Example */
+const pwd = new Password();
+
+console.log("Weak:", pwd.generateWeakPassword());
+console.log("Strong:", pwd.generateStrongPassword());
+console.log("Super Strong:", pwd.generateSuperStrongPassword());
+console.log("Funny:", pwd.generateFunnyPassword());
+
+*/
